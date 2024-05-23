c79a37aab7ff541cf7b8f995561a682c60b9804a
Matheus Verissimo <matheusmverissimo820@gmail.com>
2024-05-22 23:31:22 -0300

    QP2 - Completo

---
+++ QP2/QP2.py
@@ -0,0 +1,24 @@
+lista = []
+def escrever():
+    with open('QP2/QP2.csv', 'w') as file:
+        for i in range(6):
+            nome = input("Insira nome:")
+            file.write(f"{nome}\n")
+escrever()
+
+with open('QP2/QP2.csv', 'r') as file:
+    for i in file:
+        lista.append(i)
+
+
+
+with open('QP2/Organizado.csv', 'w') as file:
+        lista.sort()
+        for i in lista:
+            file.write(i)
+
+
+
+
+
+        
